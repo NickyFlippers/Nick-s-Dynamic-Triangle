4761137205538638b137a6726baa4b35da689743
NickyFlippers <118223056+NickyFlippers@users.noreply.github.com>
2023-03-03 15:31:32 -0500

    Initial Commit

---
+++ Nick's Dynamic Triangle/Nick_s_Dynamic_TriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nick_s_Dynamic_TriangleApp.swift
+//  Nick's Dynamic Triangle
+//
+//  Created by Nicholas Gramenos on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Nick_s_Dynamic_TriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
